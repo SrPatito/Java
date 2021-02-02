a7423d2110045b470198f9164c475cdee2aec188
SrPatito <69003176+SrPatito@users.noreply.github.com>
2021-02-01 18:35:41 -0600

    Add files via upload

---
+++ js/ejercicio1.js
@@ -0,0 +1,42 @@
+
+
+//var precio = prompt("precio")
+
+//if(precio >= 100){
+    
+    
+    
+    //var condescuento = precio - (precio*10/100);
+    
+    
+    //alert"q" + (precio + " tiene un 10% de descuento total a pagar Q : + condescuento")
+    //alert(condescuento)
+    
+    
+    
+///}else{
+    // alert("Precio completo")
+//}
+/////
+
+
+
+
+
+var user = prompt("usuario:")
+
+  if(user == "olveck"){
+      
+      var password = prompt("contraseña")
+      
+    if(password == "gatitos123"){
+        alert("hola " + user)
+    }else{
+        alert("Contraseña incorrecta")
+    
+    }
+        
+    }else{
+        alert("Usuario no encontrado")
+  }
+      
---
+++ js/programa.js
@@ -0,0 +1,38 @@
+//Esto se utiliza para realizar una alerta de ventana
+//emergente.
+
+    //alert("Ya estas conectado a JavaScript")
+
+
+//VARIABLES CON TIPOS DE DATOS
+ var datoTexto = "Hola"
+ var datoNumerico = 1234
+ var datoDecimal = 3.1416
+ var datoChar = 'a'
+ var datosLogicos = true // o false
+ 
+  //UTILIZANDO UN ALERT PARA MOSTRAR LOS TIPOS DE DATOS
+  //CON VARIABLES
+ 
+ alert(datoTexto + " como estas " + datoNumerico)
+
+ alert(datoNumerico + datoDecimal)
+ 
+
+
+var edad = "18";
+
+//<
+//<=
+//>
+//>==
+//==
+//!==
+
+var edad = prompt("Cual es tu edad");//dato a utilizar
+
+if(edad>=18){ //pregunta logica
+    alert("Eres mayor de edad") // respuesta true
+    }else{
+        alert("Eres menor de edad") // respuesta false
+    }
